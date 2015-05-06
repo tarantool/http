edae7ef0255603a62ab779fbf867a36b0becb48e
Roman Tsisyk <roman@tsisyk.com>
2015-05-06 14:26:20 +0300

    Fix CMake scripts

--- cmake/FindTarantool.cmake
+++ cmake/FindTarantool.cmake
@@ -1,11 +1,7 @@
-if(TARANTOOL_FOUND)
-    return()
-endif()
-
 macro(extract_definition name output input)
     string(REGEX MATCH "#define[\t ]+${name}[\t ]+\"([^\"]*)\""
         _t "${input}")
-    string(REGEX REPLACE "#define[\t ]+${name}[\t ]+\"([^\"]*)\"" "\\1"
+    string(REGEX REPLACE "#define[\t ]+${name}[\t ]+\"(.*)\"" "\\1"
         ${output} "${_t}")
 endmacro()
 
@@ -25,9 +21,15 @@ if (_dir)
     extract_definition(MODULE_INCLUDEDIR TARANTOOL_INCLUDEDIR ${_config})
     unset(_config)
 endif (_dir)
+unset (_dir)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(TARANTOOL
     REQUIRED_VARS TARANTOOL_INCLUDEDIR TARANTOOL_LUADIR TARANTOOL_LIBDIR
     VERSION_VAR TARANTOOL_VERSION)
+if (TARANTOOL_FOUND AND NOT TARANTOOL_FIND_QUIETLY AND NOT FIND_TARANTOOL_DETAILS)
+    set(FIND_TARANTOOL_DETAILS ON CACHE INTERNAL "Details about TARANTOOL")
+    message(STATUS "Tarantool LUADIR is ${TARANTOOL_LUADIR}")
+    message(STATUS "Tarantool LIBDIR is ${TARANTOOL_LIBDIR}")
+endif ()
 mark_as_advanced(TARANTOOL_INCLUDEDIR TARANTOOL_LUADIR TARANTOOL_LIBDIR TARANTOOL_VERSION)
