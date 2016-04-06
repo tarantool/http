9acfe05a600e3d078d9e3a54b10e64951a7615cb
bigbes <bigbes@gmail.com>
2016-04-06 15:13:01 +0300

    Add tests and add allowed_failures

--- test.sh
+++ test.sh
@@ -5,13 +5,18 @@ curl http://download.tarantool.org/tarantool/1.6/gpgkey | sudo apt-key add -
 release=`lsb_release -c -s`
 
 sudo tee /etc/apt/sources.list.d/tarantool_1_6.list <<- EOF
-deb http://download.tarantool.org/tarantool/1.6/debian/ $release main
-deb-src http://download.tarantool.org/tarantool/1.6/debian/ $release main
+deb http://download.tarantool.org/tarantool/1.6/ubuntu/ $release main
+deb-src http://download.tarantool.org/tarantool/1.6/ubuntu/ $release main
 EOF
+
 sudo apt-get update > /dev/null
 sudo apt-get -q -y install tarantool tarantool-dev
 
-# test
-cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo
-make
-make check
+# test IS
+cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo && make && make check
+
+make clean -dxff
+
+# test OOS
+mkdir build-test && cd build-test && cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo &&
+    make && make check
