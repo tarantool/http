f361368c58696daa8d203e2a856e2ba5c84c3d65
Max Melentiev <m.melentiev@corp.mail.ru>
2019-10-23 13:08:16 +0300

    Rewrite tap tests to luatest

--- test/middleware_test.lua
+++ test/middleware_test.lua
@@ -1,30 +1,9 @@
-#!/usr/bin/env tarantool
+local t = require('luatest')
+local g = t.group('middleware')
 
-local tap = require('tap')
 local middleware_module = require('http.router.middleware')
 
--- fix tap and http logs interleaving.
---
--- tap module writes to stdout,
--- http-server logs to stderr.
--- this results in non-synchronized output.
---
--- somehow redirecting stdout to stderr doesn't
--- remove buffering of tap logs (at least on OSX).
--- Monkeypatching to the rescue!
-
-local orig_iowrite = io.write
-package.loaded['io'].write = function(...)
-    orig_iowrite(...)
-    io.flush()
-end
-
-local test = tap.test("http")
-test:plan(1)
-
-test:test("ordering", function(test)
-  test:plan(7)
-
+g.test_ordering = function()
   local middleware = middleware_module.new()
 
   local add = function(opts, add_opts)
@@ -36,17 +15,17 @@ test:test("ordering", function(test)
       end
 
       local ok = middleware:use(opts)
-      test:is(ok, should_be_ok, msg)
+      t.assert_equals(ok, should_be_ok, msg)
   end
 
   local ensure_before = function(mwname1, mwname2)
       local msg = ('%s must be ordered before %s'):format(mwname1, mwname2)
       for _, mw in ipairs(middleware:ordered()) do
           if mw.name == mwname1 then
-              test:ok(true, msg)
+              -- test:ok(true, msg)
               return
           elseif mw.name == mwname2 then
-              test:fail(msg)
+              t.fail(msg)
               return
           end
       end
@@ -78,4 +57,4 @@ test:test("ordering", function(test)
   })
 
   middleware:clear()
-end)
+end
