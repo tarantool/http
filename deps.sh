a17e7103b53a73c29dc6b105bb053f3f0158f0d1
Sergey Bronnikov <sergeyb@tarantool.org>
2021-11-10 20:34:07 +0300

    Add code coverage and coveralls support
    Patch adds a support of code coverage for Lua source code and Coveralls
    support, see [1].
    
    1. https://coveralls.io/github/tarantool/http

--- deps.sh
+++ deps.sh
@@ -6,5 +6,11 @@ set -e
 
 # Test dependencies:
 tarantoolctl rocks install luatest 0.5.5
+tarantoolctl rocks install luacheck 0.25.0
+tarantoolctl rocks install luacov 0.13.0
+
+# cluacov, luacov-coveralls and dependencies
+tarantoolctl rocks install luacov-coveralls 0.2.3-1 --server=https://luarocks.org
+tarantoolctl rocks install cluacov 0.1.2-1 --server=https://luarocks.org
 
 tarantoolctl rocks make
